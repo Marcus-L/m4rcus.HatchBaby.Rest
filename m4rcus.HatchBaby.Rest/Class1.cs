839bf19ed5890d94eefbf4a2eb473283044de6df
Marcus Lum <marcusl@gmail.com>
2017-09-13 12:41:31 -0500

    Add project files.

---
+++ m4rcus.HatchBaby.Rest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m4rcus.HatchBaby.Rest
+{
+    public class Class1
+    {
+    }
+}
